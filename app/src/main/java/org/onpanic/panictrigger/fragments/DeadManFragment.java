7e20aca74cb0ee34a72ce9320e0e235bc79b58df
arrase <arrase@gmail.com>
2017-01-07 18:54:43 +0100

    deadman layout

--- app/src/main/java/org/onpanic/panictrigger/fragments/DeadManFragment.java
+++ app/src/main/java/org/onpanic/panictrigger/fragments/DeadManFragment.java
@@ -1,14 +1,19 @@
 package org.onpanic.panictrigger.fragments;
 
-import android.os.Bundle;
 import android.app.Fragment;
+import android.content.Context;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import org.onpanic.panictrigger.R;
 
 public class DeadManFragment extends Fragment {
+    private Spinner mDeadSpinner;
+    private Context mContext;
 
     public DeadManFragment() {
     }
@@ -16,6 +21,22 @@ public class DeadManFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_dead_man, container, false);
+        View v = inflater.inflate(R.layout.fragment_dead_man, container, false);
+
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(mContext,
+                R.array.time_unit, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        mDeadSpinner = (Spinner) v.findViewById(R.id.dead_time_spinner);
+        mDeadSpinner.setAdapter(adapter);
+
+        return v;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        mContext = context;
     }
 }
